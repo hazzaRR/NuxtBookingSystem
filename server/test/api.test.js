eddc88b5564833e89276d05ffa1a495e8e06011e
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2023-07-06 23:26:22 +0100

    tested login features of website

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -55,18 +55,64 @@ if (process.env.DATABASE === 'test') {
         
             const response = await testSession
             .post('/register')
-            .send({email: 'test@email.com', firstname: 'Harry', surname: 'Redman', password: 'testPassword10!', telephone: 'telephone'})
+            .send({email: 'test@email.com', firstname: 'Harry', surname: 'Redman', password: 'testPassword10!', telephone: '07450380222'})
             .expect(409);
             
             expect(response.body.message).to.equal('Register Invalid');
   
           });
 
+    });
 
+        //test logging into server with a previously created test account made above
+    describe('Account Login', () => {
+      let testSession = null;
+      let csrfToken;
+      let sessionID;
+      
+      it('Unsuccessful Log in, entering incorrect user password', async () => {
+        testSession = session(app);
+        const response = await testSession
+        .post('/login')
+        .send({ email: 'test@email.com', password: 'testPassword10'})
+        .expect(409);
+        
+        
+        expect(response.body.message).to.equal('Error with authentication');
+      });
 
+      it('Unsuccessful Log in, entering email doesn\'t exist', async () => {
+        testSession = session(app);
+        const response = await testSession
+        .post('/login')
+        .send({ email: 'test12@email.com', password: 'd10'})
+        .expect(409);
+        
+        
+        expect(response.body.message).to.equal('Error with authentication');
+      });
+      
+      it('successful Log in', async () => {
+        testSession = session(app);
+        const response = await testSession
+        .post('/login')
+        .send({ email: 'test@email.com', password: 'testPassword10!'})
+        .expect(200);
+        
+        const setCookieHeader = response.headers['set-cookie'];
+        sessionID = setCookieHeader.find(cookie => cookie.includes('auth_token='));
+        
+        csrfToken = await testSession
+        .get('/csrf-token')
+        .set('Cookie', sessionID)
+        .expect(200)
+        .then((res) => res.body.csrf_token);
 
-
-
+        const logout = await testSession
+        .get('/logout')
+        .set('Cookie', sessionID)
+        .expect(200);
+      });
     });
 
 
